2f9cccd67ab3649b9f48c652038d1151ecad57a1
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-01-24 15:29:12 +0000

    Add github notification

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,41 @@
-echo "PASS"
-exit 0
+#!/bin/bash
+set -x
+export DISPLAY=:99
+export GOVUK_APP_DOMAIN=test.gov.uk
+export GOVUK_ASSET_ROOT=http://static.test.gov.uk
+env
 
+function github_status {
+  STATUS="$1"
+  MESSAGE="$2"
+  gh-status alphagov/whitehall "$GIT_COMMIT" "$STATUS" -d "Build #${BUILD_NUMBER} ${MESSAGE}" -u "$BUILD_URL" >/dev/null
+}
+
+function error_handler {
+  trap - ERR # disable error trap to avoid recursion
+  local parent_lineno="$1"
+  local message="$2"
+  local code="${3:-1}"
+  if [[ -n "$message" ]] ; then
+    echo "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}"
+  else
+    echo "Error on or near line ${parent_lineno}; exiting with status ${code}"
+  fi
+  github_status failure "failed on Jenkins"
+  exit "${code}"
+}
+
+trap "error_handler ${LINENO}" ERR
+github_status pending "is running on Jenkins"
+
+echo "PASS"
+
+export EXIT_STATUS="0"
+
+if [ "$EXIT_STATUS" == "0" ]; then
+  github_status success "succeeded on Jenkins"
+else
+  github_status failure "failed on Jenkins"
+fi
+
+exit 0
