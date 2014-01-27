7d0bcbe3d1ba8bc79e8645f92d51e6f00c30c8ce
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-01-27 15:59:41 +0000

    [DH] Factor out repo name

--- jenkins.sh
+++ jenkins.sh
@@ -3,12 +3,14 @@ set -x
 export DISPLAY=:99
 export GOVUK_APP_DOMAIN=test.gov.uk
 export GOVUK_ASSET_ROOT=http://static.test.gov.uk
+export REPO_NAME="alphagov/specialist-frontend"
 env
 
 function github_status {
-  STATUS="$1"
-  MESSAGE="$2"
-  gh-status alphagov/whitehall "$GIT_COMMIT" "$STATUS" -d "Build #${BUILD_NUMBER} ${MESSAGE}" -u "$BUILD_URL" >/dev/null
+  REPO_NAME="$1"
+  STATUS="$2"
+  MESSAGE="$3"
+  gh-status "$REPO_NAME" "$GIT_COMMIT" "$STATUS" -d "Build #${BUILD_NUMBER} ${MESSAGE}" -u "$BUILD_URL" >/dev/null
 }
 
 function error_handler {
@@ -21,21 +23,21 @@ function error_handler {
   else
     echo "Error on or near line ${parent_lineno}; exiting with status ${code}"
   fi
-  github_status failure "failed on Jenkins"
+  github_status "$REPO_NAME" failure "failed on Jenkins"
   exit "${code}"
 }
 
 trap "error_handler ${LINENO}" ERR
-github_status pending "is running on Jenkins"
+github_status "$REPO_NAME" pending "is running on Jenkins"
 
 echo "PASS"
 
 export EXIT_STATUS="0"
 
 if [ "$EXIT_STATUS" == "0" ]; then
-  github_status success "succeeded on Jenkins"
+  github_status "$REPO_NAME" success "succeeded on Jenkins"
 else
-  github_status failure "failed on Jenkins"
+  github_status "$REPO_NAME" failure "failed on Jenkins"
 fi
 
 exit 0
