c0fe75b81b34e59ea22a911818cf9ef2dc5414b9
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-02-20 12:19:10 +0000

    Add list of todos. Refactor date display into helper.

---
+++ app/helpers/specialist_documents_helper.rb
@@ -0,0 +1,12 @@
+module SpecialistDocumentsHelper
+  
+  def nice_date_format date
+    unless date.blank?
+      string = "<time title='#{date}'>"
+      string += Date.parse(date).strftime('%-d %B %Y')
+      string += '</time>'
+      string.html_safe
+    end
+  end
+  
+end
