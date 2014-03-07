a5f4cae2a31a5f51f6ae38c9f322a3f288cf8a6b
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-03-07 13:45:30 +0000

    Fix cucumber feature
    The content api response fixture was completely out of sync with
    reality.

--- features/step_definitions/case_steps.rb
+++ features/step_definitions/case_steps.rb
@@ -2,15 +2,33 @@ Given(/^a published case$/) do
   @title = "Merger of Company A and Company B"
   @slug = slug_from_title(@title)
   @summary = "Example summary"
-  content_api_has_an_artefact(@slug, {
-    slug: @slug,
-    title: @title,
-    summary: @summary
-  })
+
+  artefact = artefact_for_slug(@slug).merge(
+    "title" => @title,
+    "details" => {
+      "need_id" => nil,
+      "business_proposition" => false,
+      "description" => nil,
+      "language" => "en",
+      "need_extended_font" => false,
+      "alternative_title" => nil,
+      "body" => "<p>Body content</p>\n",
+      "summary" => @summary,
+      "opened_date" => "2012-01-01",
+      "closed_date" => "2014-11-21",
+      "case_type" => "regulatory-references-and-appeals",
+      "case_state" => "closed",
+      "market_sector" => "distribution-and-service-industries",
+      "outcome_type" => "undertakings-in-lieu-of-reference",
+      "headers" => []
+    }
+  )
+
+  content_api_has_an_artefact(@slug, artefact)
 end
 
 When(/^I visit the case page$/) do
-  visit case_path(id: @slug)
+  visit "/#{@slug}"
 end
 
 Then(/^I should the case's content$/) do
