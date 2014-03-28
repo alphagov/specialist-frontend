4dd8f211acef3492189dbb988f282f7adc8819b4
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-03-28 16:48:55 +0000

    Flatten details and set up delegators

--- app/presenters/document_presenter.rb
+++ app/presenters/document_presenter.rb
@@ -4,6 +4,8 @@ class DocumentPresenter
 
   delegate :title, :details, :updated_at, to: :document
 
+  delegate :opened_date, :closed_date, :market_sector, :case_type, :case_state, :outcome_type, :summary, :headers, :body, to: :"document.details"
+
   def initialize(document)
     @document = document
   end
