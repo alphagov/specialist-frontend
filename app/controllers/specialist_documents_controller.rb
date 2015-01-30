de7f6a18d955dc8dc74015a5a2e25f83a3a9102f
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-01-30 15:34:22 +0000

    Refactor SpecialistDocumentsController
    First, we get the Finder content_item from the first part of the path.
    We then pass this into the DocumentPresenter to use. This allows us to
    get rid of all the code checking the slug and initialising a format
    Presenter class, apart from DSU (again).

--- app/controllers/specialist_documents_controller.rb
+++ app/controllers/specialist_documents_controller.rb
@@ -7,7 +7,7 @@ class SpecialistDocumentsController < ApplicationController
     artefact = content_api.artefact(params[:path])
 
     if artefact
-      @document = document_presenter(artefact)
+      @document = document(finder, artefact)
     else
       error_not_found
     end
@@ -15,39 +15,21 @@ class SpecialistDocumentsController < ApplicationController
 
 private
 
-  def finder(slug)
-    Finder.new(content_store.content_item("/#{slug}"))
+  def document(finder, artefact)
+    case artefact.format
+    when "drug_safety_update"
+      DrugSafetyUpdatePresenter.new(finder, artefact)
+    else
+      DocumentPresenter.new(finder, artefact)
+    end
+  end
+
+  def finder
+    Finder.new(content_store.content_item("/#{params[:path].split('/')[0]}"))
   end
 
   def error_not_found
     render status: :not_found, text: "404 error not found"
-  end
-
-  def document_presenter(artefact)
-    case artefact.format
-    when "aaib_report"
-      AaibReportPresenter.new(finder("aaib-reports"), artefact)
-    when "cma_case"
-      CmaCasePresenter.new(finder("cma-cases"), artefact)
-    when "drug_safety_update"
-      DrugSafetyUpdatePresenter.new(finder("drug-safety-update"), artefact)
-    when "international_development_fund"
-      InternationalDevelopmentFundPresenter.new(finder("international-development-funding"), artefact)
-    when "maib_report"
-      MaibReportPresenter.new(finder("maib-reports"), artefact)
-    when "medical_safety_alert"
-      MedicalSafetyAlertPresenter.new(finder("drug-device-alerts"), artefact)
-    when "raib_report"
-      RaibReportPresenter.new(finder("raib-reports"), artefact)
-    else
-      DocumentPresenter.new(NullSchema.new, artefact)
-    end
-  end
-
-  class NullSchema
-    def user_friendly_values(input)
-      input
-    end
   end
 
   def set_robots_headers
--- spec/controllers/specialist_documents_controller_spec.rb
+++ spec/controllers/specialist_documents_controller_spec.rb
@@ -1,46 +1,27 @@
 require 'spec_helper'
+require 'gds_api/test_helpers/content_store'
+include GdsApi::TestHelpers::ContentStore
 
 describe SpecialistDocumentsController do
 
   describe "GET 'show'" do
     let(:finder) { double(:finder) }
     let(:content_api) { double(:content_api) }
+    let(:format) { "aaib_report" }
     let(:artefact) { double(:artefact, format: format) }
-    let(:slug) { "/some-url/to-somewhere" }
+    let(:content_item) { double(:content_item) }
+    let(:slug) { "aaib-reports/an-air-accident" }
 
     before do
       allow(controller).to receive(:finder) { finder }
+      allow(finder).to receive(:document_type) { "document_type" }
       allow(controller).to receive(:content_api) { content_api }
       allow(content_api).to receive(:artefact).with(slug) { artefact }
+      content_store_has_item('/aaib-reports', content_item)
       get :show, path: slug
     end
 
-    context "when the document_type is cma_case" do
-      let(:format) { "cma_case" }
-
-      let(:cma_cases_content_item) { double(:cma_cases_content_item) }
-
-      it "sets document with a CmaCasePresenter" do
-        expect(controller).to have_received(:finder).with("cma-cases")
-        assigns(:document).should be_a(CmaCasePresenter)
-      end
-    end
-
-    context "when the document_type is aaib_report" do
-      let(:format) { "aaib_report" }
-
-      let(:aaib_reports_content_item) { double(:aaib_reports_content_item) }
-
-      it "sets document with a AaibReportPresenter" do
-        expect(controller).to have_received(:finder).with("aaib-reports")
-        assigns(:document).should be_a(AaibReportPresenter)
-      end
-    end
-
     context "excluded from search engines" do
-      let(:format) { "aaib_report" }
-      let(:slug) { "aaib-reports/an-air-accident" }
-
       it "sets the correct header" do
         expect(response.headers["X-Robots-Tag"]).should eq("none")
       end
