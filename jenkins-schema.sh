c069a74eaf5318a8afcb48b36fa28ce0fc9cfa7d
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-08-16 15:11:48 +0000

    Use gov.uk rather than co.uk for the test domains
    The .co.uk gtld does not match the test helpers in slimmer which stub out requests to static.
    
    https://github.com/alphagov/slimmer/blob/868095a99fdf559cdad80b9102d882322214bc60/lib/slimmer/test_helpers/shared_templates.rb

--- jenkins-schema.sh
+++ jenkins-schema.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 export DISPLAY=:99
-export GOVUK_APP_DOMAIN=test.alphagov.co.uk
-export GOVUK_ASSET_ROOT=http://static.test.alphagov.co.uk
+export GOVUK_APP_DOMAIN=test.alphagov.gov.uk
+export GOVUK_ASSET_ROOT=http://static.test.alphagov.gov.uk
 env
 
 function github_status {
