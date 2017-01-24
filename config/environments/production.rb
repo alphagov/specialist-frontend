4d66bb79eaeafdebb9e7139c7610450f67cd5f5e
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-01-24 15:49:48 +0000

    Make production.rb consistent with government-frontend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,16 +75,13 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  config.logstasher.enabled = true
-  config.logstasher.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.json.log")
-  config.logstasher.suppress_app_log = true
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new($stderr))
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
+  $real_stdout = $stdout.clone
+  $stdout.reopen($stderr)
+  config.logstasher.enabled = true
+  config.logstasher.logger = Logger.new($real_stdout)
+  config.logstasher.suppress_app_log = true
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
